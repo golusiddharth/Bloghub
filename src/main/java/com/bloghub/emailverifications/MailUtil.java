8effa9c886153ca536d23bd34449831a60bbc647
Golu <golusiddharth88@gmail.com>
2026-06-23 02:19:31 +0530

    fix pom and mail dependencies

--- src/main/java/com/bloghub/emailverifications/MailUtil.java
+++ src/main/java/com/bloghub/emailverifications/MailUtil.java
@@ -3,50 +3,26 @@ package com.bloghub.emailverifications;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+
 public class MailUtil {
 
     private static final String USERNAME = System.getenv("MAIL_USERNAME");
     private static final String PASSWORD = System.getenv("MAIL_PASSWORD");
 
-    // private static Session getSession() {
-    //     Properties prop = new Properties();
-    //     prop.put("mail.smtp.host", "smtp.gmail.com");
-    //     prop.put("mail.smtp.port", "465");
-    //     prop.put("mail.smtp.auth", "true");
-    //     prop.put("mail.smtp.socketFactory.port", "465");
-    //     prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-    //     return Session.getInstance(prop, new Authenticator() {
-    //         protected PasswordAuthentication getPasswordAuthentication() {
-    //             return new PasswordAuthentication(USERNAME, PASSWORD);
-    //         }
-    //     });
-    // }
     private static Session getSession() {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-    Properties prop = new Properties();
-
-    prop.put("mail.smtp.host", "smtp.gmail.com");
-    prop.put("mail.smtp.port", "587");
-    prop.put("mail.smtp.auth", "true");
-    prop.put("mail.smtp.starttls.enable", "true");
-
-    prop.put("mail.smtp.connectiontimeout", "10000");
-    prop.put("mail.smtp.timeout", "10000");
-    prop.put("mail.smtp.writetimeout", "10000");
-    prop.put("mail.debug", "true");
-
-    return Session.getInstance(prop,
-            new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(
-                            USERNAME,
-                            PASSWORD
-                    );
-                }
-            });
-}
+        return Session.getInstance(prop, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        });
+    }
 
 public static void sendOTP(String toEmail, String otp)
         throws MessagingException {
@@ -55,10 +31,7 @@ public static void sendOTP(String toEmail, String otp)
     System.out.println("PASSWORD NULL = " + (PASSWORD == null));
     System.out.println("TO EMAIL = " + toEmail);
 
-   Session session = getSession();
-    session.setDebug(true);
-
-    Message message = new MimeMessage(session);
+    Message message = new MimeMessage(getSession());
 
     message.setRecipients(
             Message.RecipientType.TO,
@@ -68,21 +41,11 @@ public static void sendOTP(String toEmail, String otp)
     message.setText("Your OTP is: " + otp);
 
         try {
-
-            System.out.println("TRYING SMTP CONNECTION...");
-
-            Transport.send(message);
-
-            System.out.println("MAIL SENT SUCCESSFULLY");
-
-        } catch (Exception e) {
-
-            System.out.println("MAIL ERROR = " + e.getMessage());
-
-            e.printStackTrace();
-
-            throw new MessagingException(e.getMessage());
-        }
+        Transport.send(message);
+        System.out.println("MAIL SENT SUCCESSFULLY");
+    } catch (Exception e) {
+        System.out.println("MAIL ERROR:");
+        e.printStackTrace();
+    }
 }
-
 }
